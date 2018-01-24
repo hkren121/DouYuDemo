2ff4ed362e59040d19faa6a180fcba3f4f3f4aec
hc <hc@hcdeiMac.local>
2018-01-24 15:52:46 +0800

    标题和内容的联动

--- DYDemo/DYDemo/Classes/Home/Controller/HomeViewController.swift
+++ DYDemo/DYDemo/Classes/Home/Controller/HomeViewController.swift
@@ -13,14 +13,15 @@ private var KTitleViewH : CGFloat = 40
 class HomeViewController: UIViewController {
     
     //MARK:- 懒加载属性
-    private lazy var pageTitleView : PageTitleView = {
+    private lazy var pageTitleView : PageTitleView = {[weak self] in
         let titleFrame = CGRect(x: 0, y: KStatusBarH + KNavigetionBarH, width: KScreenW, height: KTitleViewH)
         let titles = ["推荐", "游戏", "娱乐", "趣味"]
         let titleView = PageTitleView(frame: titleFrame, titles: titles)
+        titleView.delegate = self
         return titleView
     }()
     
-    private lazy var pageContentView : PageContentView = {
+    private lazy var pageContentView : PageContentView = { [weak self] in
         //1.确定内容的frame
         let contentH = KScreenH - KStatusBarH - KNavigetionBarH - KTitleViewH
         let contentFrame = CGRect(x: 0, y:KStatusBarH + KNavigetionBarH + KTitleViewH, width: KScreenW, height: contentH)
@@ -34,6 +35,7 @@ class HomeViewController: UIViewController {
         }
         
         let contentView = PageContentView(frame: contentFrame, childVcs: childVcs, parentViewController: self)
+        contentView.delegate = self
         return contentView
     }()
     
@@ -76,5 +78,19 @@ extension HomeViewController{
         let qrcodeItem = UIBarButtonItem(imageName: "Image_scan", highImageName: "Image_scan_click", size: size)
         
         navigationItem.rightBarButtonItems = [historyItem, searchItem, qrcodeItem]
+    }
+}
+
+//MARK:- 遵守PageTitleViewDelegate协议
+extension HomeViewController : PageTitleViewDelegate{
+    func pageTitleView(titleView: PageTitleView, selectedIndex index: Int) {
+        pageContentView.setCurrentIndex(currentIndex: index)
+    }
+}
+
+//MARK:- 遵守PageContentViewDelegate协议
+extension HomeViewController : PageContentViewDelegate {
+    func pageContentView(contentView: PageContentView, progress: CGFloat, sourceIndex: Int, targetIndex: Int) {
+        pageTitleView.setTitleWithProgress(progress: progress, sourceIndex: sourceIndex, targetIndex: targetIndex)
     }
 }
--- DYDemo/DYDemo/Classes/Main/View/PageTitleView.swift
+++ DYDemo/DYDemo/Classes/Main/View/PageTitleView.swift
@@ -8,11 +8,22 @@
 
 import UIKit
 
+//MARK:- 定义协议
+protocol PageTitleViewDelegate : class {
+    func pageTitleView(titleView : PageTitleView, selectedIndex index : Int)
+}
+
+//MARK:- 定义常量
+private let KScrollLineH : CGFloat = 2
+private let KNormalColor : (CGFloat, CGFloat, CGFloat) = (85, 85, 85)
+private let KSelectColor : (CGFloat, CGFloat, CGFloat) = (255, 128, 0)
+
 class PageTitleView: UIView {
     
     //MARK:- 定义属性
+    private var currentIndex : Int = 0
     private var titles : [String]
-    let KScrollLineH : CGFloat = 2
+    weak var delegate : PageTitleViewDelegate?
     
     //MARK:- 懒加载属性
     private lazy var titleLabels : [UILabel] = [UILabel]()
@@ -76,15 +87,21 @@ extension PageTitleView{
             label.text = title
             label.tag = index
             label.font = UIFont.systemFont(ofSize: 16.0)
-            label.textColor = UIColor.darkGray
+            label.textColor = UIColor(r: KNormalColor.0, g: KNormalColor.1, b: KNormalColor.2)
             label.textAlignment = .center
             
             //3.设置label的frame
             let labelX : CGFloat = labelW * CGFloat(index)
             label.frame = CGRect(x: labelX, y: labelY, width: labelW, height: labelH)
             
+            //4、将label添加到scrollView中
             scrollView.addSubview(label)
             titleLabels.append(label)
+            
+            //5、给label添加手势
+            label.isUserInteractionEnabled = true
+            let tapGes = UITapGestureRecognizer(target: self, action: #selector(titleLabelClick(tapGes:)))
+            label.addGestureRecognizer(tapGes)
         }
     }
     
@@ -99,9 +116,75 @@ extension PageTitleView{
         //2.添加scrollLine
         //2.1获取第一个Label
         guard let firstLabel = titleLabels.first else { return }
-        firstLabel.textColor = UIColor.orange
+        firstLabel.textColor = UIColor(r: KSelectColor.0, g: KSelectColor.1, b: KSelectColor.2)
         
         scrollView.addSubview(scrollLine)
         scrollLine.frame = CGRect(x: firstLabel.frame.origin.x, y: frame.height - KScrollLineH, width: firstLabel.frame.width, height: KScrollLineH)
+    }
+}
+
+//MARK:- 监听Label的打击
+extension PageTitleView{
+    @objc private func titleLabelClick(tapGes : UITapGestureRecognizer){
+        //1.获取当前label
+        guard let currentLabel = tapGes.view as? UILabel else { return }
+        
+        if currentIndex == currentLabel.tag {
+            return
+        }
+        
+        //2、获取之前的Label
+        let oldLabel = titleLabels[currentIndex]
+        
+        //3、切换文字的颜色
+        oldLabel.textColor = UIColor(r: KNormalColor.0, g: KNormalColor.1, b: KNormalColor.2)
+        currentLabel.textColor = UIColor(r: KSelectColor.0, g: KSelectColor.1, b: KSelectColor.2)
+        
+        //4、保存最新Label的下标值
+        currentIndex = currentLabel.tag
+        
+        //5、滚动条位置发生变化
+        let scrollLineX = CGFloat(currentIndex) * scrollLine.frame.width
+        UIView.animate(withDuration: 0.15) {
+            self.scrollLine.frame.origin.x = scrollLineX
+        }
+        
+        //6、通知代理
+        delegate?.pageTitleView(titleView: self, selectedIndex: currentIndex)
+    }
+}
+
+//MARK:- 对外暴露的方法
+extension PageTitleView{
+    func setTitleWithProgress(progress : CGFloat, sourceIndex : Int, targetIndex : Int){
+        //1、取出sourceLabel/targetLabel
+        let sourceLabel = titleLabels[sourceIndex]
+        let targetLabel = titleLabels[targetIndex]
+        
+        //2、处理滑块的逻辑
+        let moveTotalX = targetLabel.frame.origin.x - sourceLabel.frame.origin.x
+        let moveX = moveTotalX * progress
+        scrollLine.frame.origin.x = sourceLabel.frame.origin.x + moveX
+        
+        //3、颜色的渐变
+        //3.1、取出变化的范围
+        let colorDelta = (KSelectColor.0 - KNormalColor.0, KSelectColor.1 - KNormalColor.1, KSelectColor.2 - KNormalColor.2)
+        
+        //3.2、变化sourcelabel
+        sourceLabel.textColor = UIColor(r: KSelectColor.0 - colorDelta.0 * progress, g: KSelectColor.1 - colorDelta.1 * progress, b: KSelectColor.2 - colorDelta.2 * progress)
+        
+        //3.3、变化targetlabel
+        targetLabel.textColor = UIColor(r: KNormalColor.0 + colorDelta.0 * progress, g: KNormalColor.1 + colorDelta.1 * progress, b: KNormalColor.2 + colorDelta.2 * progress)
+        
+        if sourceIndex == targetIndex {
+            for index in 0...3{
+                let lab = titleLabels[index]
+                lab.textColor = UIColor(r: KNormalColor.0, g: KNormalColor.1, b: KNormalColor.2)
+            }
+            targetLabel.textColor = UIColor(r: KSelectColor.0, g: KSelectColor.1, b: KSelectColor.2)
+        }
+        
+        //4、记录最新的index
+        currentIndex = targetIndex
     }
 }
